ac80cddf9d004637df56eee273f64fd671c6e947
montanker <tim.drx.house@gmail.com>
2015-01-20 21:24:58 -0500

    Minor tweaks

--- Assignment 0/Assignment 0/main.cpp
+++ Assignment 0/Assignment 0/main.cpp
@@ -12,6 +12,7 @@
 
 #include "Cube.h"
 #include "Camera.h"
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <iostream>
@@ -46,7 +47,7 @@ int main(int argc, const char* argv[])
 	GLFWwindow* window;
 
 	glfwSetErrorCallback(error_callback);
-
+	
 	if (!glfwInit())
 	{
 		fprintf( stderr, "Failed to initialize GLFW\n" );
@@ -90,13 +91,9 @@ int main(int argc, const char* argv[])
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glMatrixMode(GL_PROJECTION);
 		camera->draw();
-		//gluPerspective(45.0f, ratio, 0.1f, 100.f);
 		//glRotatef((float) glfwGetTime() * 50.f, 0.0f, 1.0f, 1.0f);
 		
 		glMatrixMode(GL_MODELVIEW);
-		/*glRotatef(camera->mCamRotX, 1.0f, 0.0f, 0.0f);
-		glRotatef(camera->mCamRotY, 0.0f, 1.0f, 0.0f);
-		glTranslatef(-camera->mCamX, -camera->mCamY, -camera->mCamZ);*/
 		cube->draw();
 
 		glfwSwapBuffers(window);
@@ -107,29 +104,3 @@ int main(int argc, const char* argv[])
 	glfwTerminate();
 	exit(EXIT_SUCCESS);
 }
-
-	/*while (!glfwWindowShouldClose(window))
-	{
-		float ratio;
-		int width, height;
-		glfwGetFramebufferSize(window, &width, &height);
-		ratio = width / (float) height;
-		glViewport(0, 0, width, height);
-		glClear(GL_COLOR_BUFFER_BIT);
-		glMatrixMode(GL_PROJECTION);
-		glLoadIdentity();
-		glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
-		glMatrixMode(GL_MODELVIEW);
-		glLoadIdentity();
-		glRotatef((float) glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
-		glBegin(GL_TRIANGLES);
-		glColor3f(1.f, 0.f, 0.f);
-		glVertex3f(-0.6f, -0.4f, 0.f);
-		glColor3f(0.f, 1.f, 0.f);
-		glVertex3f(0.6f, -0.4f, 0.f);
-		glColor3f(0.f, 0.f, 1.f);
-		glVertex3f(0.f, 0.6f, 0.f);
-		glEnd();
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}*/
